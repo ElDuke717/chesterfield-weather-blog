256157d9f2271a0490e782c3f7f097753afc9529
elduke717 <spacematcha@proton.me>
2023-10-30 09:49:08 -0400

    remove secret code snippet to see if code works

--- src/WeatherPage.js
+++ src/WeatherPage.js
@@ -1,9 +1,14 @@
 // WeatherPage.js
+
 import React, { useState, useEffect } from "react";
+
+// code snippet added to manage secrets
 import {
   SecretsManagerClient,
   GetSecretValueCommand,
 } from "@aws-sdk/client-secrets-manager";
+
+
 const MAC_ADDRESS = process.env.REACT_APP_MAC_ADDRESS;
 const AW_API_KEY = process.env.REACT_APP_AW_API_KEY;
 const APP_KEY = process.env.REACT_APP_APP_KEY;
@@ -12,30 +17,30 @@ const APP_KEY = process.env.REACT_APP_APP_KEY;
 // If you need more information about configurations or implementing the sample code, visit the AWS docs:
 // https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/getting-started.html
 
-const secret_name = "weather_app_keys";
+// const secret_name = "weather_app_keys";
 
-const client = new SecretsManagerClient({
-  region: "us-east-1",
-});
+// const client = new SecretsManagerClient({
+//   region: "us-east-1",
+// });
 
-let response;
+// let response;
 
-try {
-  response = await client.send(
-    new GetSecretValueCommand({
-      SecretId: secret_name,
-      VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
-    })
-  );
-} catch (error) {
-  // For a list of exceptions thrown, see
-  // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-  throw error;
-}
+// try {
+//   response = await client.send(
+//     new GetSecretValueCommand({
+//       SecretId: secret_name,
+//       VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+//     })
+//   );
+// } catch (error) {
+//   // For a list of exceptions thrown, see
+//   // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+//   throw error;
+// }
 
-const secret = response.SecretString;
+// const secret = response.SecretString;
 
-// Your code goes here
+// Weather page component
 
 const WeatherPage = () => {
   // State for weather and sun data
